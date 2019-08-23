a77887c77764dec2a94e34cfb45f31a553d3db71
Robert Sotomski <sotomski@gmail.com>
2019-08-23 15:13:05 +0200

    ignore ssl

--- services/http.go
+++ services/http.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -11,6 +12,7 @@ import (
 var myClient = &http.Client{Timeout: 10 * time.Second}
 
 func GetJson(url string, target interface{}) error {
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	log.Printf("Request: %s", url)
 	r, err := myClient.Get(url)
 	if err != nil {
